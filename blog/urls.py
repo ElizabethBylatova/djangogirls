3d598b7e391dceed76df367cb5b60a2f5040358e
Your Name <you@example.com>
2019-05-28 17:28:14 +0300

    Changed views.post_list

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.post_list, name='post_list'),
+    path('', views.PostList.as_view(), name='post_list'),
     path('post/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
     path('post/new/', views.PostNew.as_view(), name='post_new'),
     path('post/<int:pk>/edit/', views.DetailView.as_view(), name='post_edit'),
