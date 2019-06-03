b0368b50f8ee14420e576416a02d7a22f0ee31cd
Your Name <you@example.com>
2019-06-03 16:19:15 +0300

    Created folders static_in_dev, media. Activity bootstrap. Set the class buttons btn bth-primary

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,3 +120,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFIELS_DIRS = (
+
+        os.path.join(BASE_DIR, 'static_in_dev'),
+)
