df79c8d11324569885bef2ba09beb2f186556d2b
Your Name <you@example.com>
2019-06-04 16:33:02 +0300

    Fixed bug

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,7 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFIELS_DIRS = (
+STATICFILES_DIRS = (
 
         os.path.join(BASE_DIR, 'static_in_dev'),
 )
